462febf69403a60d28ed5d3cf81396c4500726c2
Rohit Darunkar <139346362+rohitdarunkar@users.noreply.github.com>
2026-01-16 23:08:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function validEmail(str) {
-  //your JS code here.
+  if (!str) return false;
+
+  const emailRegex =
+    /^[a-zA-Z0-9]+([.-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-]?[a-zA-Z0-9]+)*(\.[a-zA-Z]{2,3})+$/;
+
+  return emailRegex.test(str);
 }
+
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
